f76506e212ac94907d8b66dadf27ac522a64013b
Jack Patil <amglobal99@yahoo.com>
2020-04-06 20:06:33 -0400

    added browsing state

---
+++ MAIN/StateManagement/BrowsingState.swift
@@ -0,0 +1,53 @@
+//
+//  BrowsingState.swift
+//  MAIN
+//
+//  Created by amglobal on 4/6/20.
+//  Copyright © 2020 Natsys. All rights reserved.
+//
+
+import Foundation
+
+
+
+//MARK:- Browsing State
+
+struct BrowsingState {
+    
+    var vehicleNumber: String?
+    
+    init(vehicleNumber: String?) {
+        self.vehicleNumber = vehicleNumber
+    }
+    
+}
+
+
+//MARK:- Extension
+
+
+extension BrowsingState {
+
+
+
+
+}
+
+
+
+
+
+//MARK:- REDUCER
+
+/*
+func browsingStateActionReducer(_ action: Action, state: BrowsingState, route: Cordux.Route) -> (InitializationKind, Cordux.Route) {
+    let browsingState = BrowsingState(vehicleNumber: state.vehicleNumber)
+    return (.initialized(.authenticated(browsingState)), route)
+}
+
+*/
+
+
+
+
+
